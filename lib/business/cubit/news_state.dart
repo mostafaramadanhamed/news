fad3a4c6f4d3c4b421444d45465a4049d35951d1
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-05 10:26:47 +0200

    add all states

--- lib/business/cubit/news_state.dart
+++ lib/business/cubit/news_state.dart
@@ -1,2 +1,11 @@
 abstract class NewsStates{}
 class NewsInitialState extends NewsStates{}
+class NewsLoadingState extends NewsStates{}
+class TopNewsSuccessState extends NewsStates{}
+class TopNewsFailureState extends NewsStates{}
+class BusinessNewsSuccessState extends NewsStates{}
+class BusinessNewsFailureState extends NewsStates{}
+class SportsNewsSuccessState extends NewsStates{}
+class SportsNewsFailureState extends NewsStates{}
+class SciencesNewsSuccessState extends NewsStates{}
+class SciencesNewsFailureState extends NewsStates{}

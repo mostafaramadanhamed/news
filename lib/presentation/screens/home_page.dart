8bef65816663f897d6382eea2b68c2e2f3a9ee07
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-09 08:27:21 +0200

    create home page

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -1,12 +1,44 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../business/cubit/news_cubit.dart';
+import '../../business/cubit/news_state.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return BlocConsumer<NewsCubit, NewsStates>(
+      listener:(context,state){} ,
+      builder: (context,state){
+        var cubit=NewsCubit.get(context);
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+                'News App'
+            ),
+            actions: [
+              IconButton(onPressed:(){
+
+              } , icon:Icon(Icons.search) ),
+              IconButton(onPressed: (){
+
+              }, icon: Icon(Icons.brightness_4_outlined))
+            ],
+          ),
+          body: cubit.screens[cubit.currentIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            currentIndex: cubit.currentIndex,
+            onTap: (index){
+              cubit.changeBottomNavBar(index);
+            },
+            items: cubit.bottomItems,
+          ),
+        );
+      },
+
     );
   }
 }

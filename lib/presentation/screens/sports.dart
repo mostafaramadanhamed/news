b99787edae876ee0bd78114ecdea59abd679a580
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-10 06:08:52 +0200

    finish sports screen

--- lib/presentation/screens/sports.dart
+++ lib/presentation/screens/sports.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news/business/cubit/news_cubit.dart';
+import 'package:news/presentation/widgets/article.dart';
+class SportsScreen  extends StatelessWidget {
+  const SportsScreen({Key? key}) : super(key: key);
 
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer(
+      listener: (context,state){},
+      builder: (context,state){
+        var list=NewsCubit.get(context).sports;
+        return articleBuilder(list, context);
+      },
+    );
+  }
+}

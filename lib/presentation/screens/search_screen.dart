6f506efd2ae548007ef040efedfd653c7bb742f8
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-11 09:07:05 +0200

    create custom text field

--- lib/presentation/screens/search_screen.dart
+++ lib/presentation/screens/search_screen.dart
@@ -5,6 +5,18 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return  Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        height: MediaQuery.of(context).size.height/14,
+        width: MediaQuery.of(context).size.width,
+        margin: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/30,vertical: MediaQuery.of(context).size.height/30),
+        padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width/60,vertical: MediaQuery.of(context).size.height/80),
+        decoration: BoxDecoration(
+          color: Colors.grey,
+        ),
+        child: TextField(),
+      ),
+    );
   }
 }
---
+++ lib/presentation/widgets/custom_text_field.dart
@@ -0,0 +1,57 @@
+
+import 'package:flutter/material.dart';
+class CustomTextFiled extends StatelessWidget {
+  const CustomTextFiled({Key? key,
+    this.onChange ,
+    required this.hint,
+    this.maxLines=1,
+    this.onSaved,
+  }) : super(key: key) ;
+
+  final String ?hint;
+  final void Function(String?)? onSaved;
+  final Function(String) ? onChange;
+  final int maxLines;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextFormField(
+        onChanged: onChange,
+        onSaved: onSaved,
+        validator: (val){
+          if(val?.isEmpty??true){
+            return 'Field is required';
+          }
+          else {
+            return null;
+          }
+        },
+        maxLines: maxLines,
+        cursorColor: Colors.black,
+        decoration: InputDecoration(
+          //  fillColor: SBackgroundColor,
+            hintText: hint,
+            hintStyle:const TextStyle(
+                color: Colors.black
+            ),
+            errorBorder:buildBorder(border: 16, color: Colors.red),
+            focusedBorder: buildBorder(border: 16, color: Colors.black),
+            enabledBorder: buildBorder(border: 16, color: Colors.white),
+            border:buildBorder(border: 16, color: Colors.white)
+        ),
+      ),
+    );
+  }
+
+  OutlineInputBorder buildBorder({
+    required double border,
+    required Color color,
+  }) {
+    return OutlineInputBorder(
+        borderSide:  BorderSide(
+          color: color,
+        ),
+        borderRadius: BorderRadius.circular(border)
+    );
+  }
+}

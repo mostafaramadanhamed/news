9804cd9e9924595092f2a7370bcdfcb46934b4b1
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-11 06:55:57 +0200

    add flutter offline

--- lib/constants/images.dart
+++ lib/constants/images.dart
@@ -1,4 +1,5 @@
 class MyImages{
   static const loadingImg='assets/images/loading.gif';
   static const nullImg='assets/images/nullimage.png';
+  static const noInternetImg='assets/images/no_internet.jpg';
 }
--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -1,45 +1,95 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_offline/flutter_offline.dart';
+import 'package:news/constants/images.dart';
 
 import '../../business/cubit/news_cubit.dart';
 import '../../business/cubit/news_state.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
+  Widget buildNoInternetWidget(context){
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height/15,
+          ),
+          Image.asset(MyImages.noInternetImg,
+            height:MediaQuery.of(context).size.height/2 ,
+            width: MediaQuery.of(context).size.width,),
+           Text(
+            ' Can\'t connect.. check your internet',
+            style: TextStyle(
+              fontSize: MediaQuery.of(context).size.width/20,
+              color: Colors.black,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocConsumer<NewsCubit, NewsStates>(
-      listener:(context,state){} ,
-      builder: (context,state){
-        var cubit=NewsCubit.get(context);
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text(
-                'News App'
-            ),
-            actions: [
-              IconButton(onPressed:(){
-
-              } , icon:const Icon(Icons.search) ),
-              IconButton(onPressed: (){
-
-              }, icon: const Icon(Icons.brightness_4_outlined))
-            ],
-          ),
-          body: cubit.screens[cubit.currentIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            selectedItemColor: Colors.deepOrange,
-            unselectedItemColor: Colors.orange,
-            currentIndex: cubit.currentIndex,
-            onTap: (index){
-              cubit.changeBottomNavBar(index);
-            },
-            items: cubit.bottomItems,
-          ),
+      listener:(context,state){
+      } ,
+      builder: (context,state) {
+        var cubit = NewsCubit.get(context);
+        return  OfflineBuilder(
+            connectivityBuilder: (
+                BuildContext context,
+                ConnectivityResult connectivity,
+                Widget child,
+                )
+        {
+          final bool connected = connectivity != ConnectivityResult.none;
+          if(connected){
+            return  Scaffold(
+                appBar: buildAppBar(),
+                bottomNavigationBar: BottomNavigationBar(
+                  items:cubit.bottomItems,
+                  currentIndex: cubit.currentIndex,
+                  onTap: (index){
+                    cubit.changeBottomNavBar(index);
+                  },
+                ),
+                body: cubit.screens[cubit.currentIndex]
+            );
+          }
+          else{
+            return Scaffold(
+              body: buildNoInternetWidget(context),
+            );
+          }
+        },
+          child: Center(child: CircularProgressIndicator(),),
         );
-      },
-
+      }
     );
+  }
+
+
+  AppBar buildAppBar() {
+    return AppBar(
+          title: const Text(
+              'News App'
+          ),
+          actions: [
+            IconButton(onPressed:(){
+
+            } , icon:const Icon(Icons.search) ),
+            IconButton(onPressed: (){
+
+            }, icon: const Icon(Icons.brightness_4_outlined))
+          ],
+        );
   }
 }

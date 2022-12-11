5d0053a92fe5099e326ec5d0386635d6b158eb0b
mostafaramadanhamed <mostafaramadanhamed010@gmail.com>
2022-12-11 07:00:36 +0200

    edit bottom nav bar

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -55,6 +55,8 @@ class HomeScreen extends StatelessWidget {
             return  Scaffold(
                 appBar: buildAppBar(),
                 bottomNavigationBar: BottomNavigationBar(
+                  selectedItemColor: Colors.deepPurple,
+                  unselectedItemColor: Colors.deepPurpleAccent.shade100,
                   items:cubit.bottomItems,
                   currentIndex: cubit.currentIndex,
                   onTap: (index){
